d6beca1f52a9801e9058ce572c49734ee9a4ccd8
nikos <delisnikos95@gmail.com>
2019-09-30 21:24:13 +0300

    changes

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -11,6 +11,6 @@ router.get("/:orderId",checkAuth,OrdersCOntroller.orders_get_order);
 
 router.delete("/:orderId",checkAuth,OrdersCOntroller.orders_delete_order);
 
-router.get("/:customerId",checkAuth,OrdersCOntroller.get_user_orders);
+router.get("/customer/:customerId",checkAuth,OrdersCOntroller.get_user_orders);
 
 module.exports=router;
